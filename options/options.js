ae8b68905b99593d0d7bd50923b6b4ff8175a8e3
Crowdbotics <team@crowdbotics.com>
2021-09-14 01:18:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "patient_wildflower_30528Identifier",
+  url: "https://patient_wildflower_30528Identifier.botics.co",
+  api: "https://patient_wildflower_30528Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
